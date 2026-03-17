e0bf02cc8bcf7dbcfc90a7bbebabde4d6444a5cb
karthi3075 <karthijayakumar3007@gmail.com>
2026-03-17 22:27:00 +0530

    chance card color

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const MealCard = ({meal}) => {
   return (
-    <div className='border border-orange-500 rounded-md hover:scale-105 bg-blue-300 duration-200'>
+    <div className='border border-orange-500 rounded-md hover:scale-105 bg-gray-300 duration-200'>
         <div className=''>
             <img src={meal.strMealThumb} alt="Meal is Loading..." className='w-full h-50 object-cover' />
         </div>
